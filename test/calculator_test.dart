8b6be9e17dfbf770677bac4b891036e5e29f6ef0
khurram <kf115462@gmail.com>
2021-12-31 17:06:18 +0500

    Widget_Testing_Example

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,56 +1,51 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'calculator.dart';
 
-
-void main(){
-
-
+void main() {
   late Calculator calculator;
 
-
   // This Function runs first of all
-  setUpAll((){
-    calculator=Calculator();
-    print("Testing Started");
+  setUpAll(() {
+    calculator = Calculator();
+    if (kDebugMode) {
+      print("Testing Started");
+    }
   });
 
   // Runs After every test
-  setUp((){
-    print("Before  Test");
+  setUp(() {
+    if (kDebugMode) {
+      print("Before  Test");
+    }
   });
-
-
-
 
   // This functions run at the end
-  tearDownAll((){
-    print("Testing Completed");
+  tearDownAll(() {
+    if (kDebugMode) {
+      print("Testing Completed");
+    }
   });
-
-
 
   // Runs After every test case
-  tearDown((){
-    print("After Test");
+  tearDown(() {
+    if (kDebugMode) {
+      print("After Test");
+    }
   });
 
+  group("Calculator Module", () {
+    test("Test Add functionality of calculator", () {
+      int result = calculator.add(4, 5);
+      expect(result, 9);
+    });
 
-
-   group("Calaculator Module", (){
-
-     test("Test Add functionaliy of calculator",(){
-       int result=calculator.add(4, 5);
-       expect(result, 9);
-     });
-
-     test("Test Multiplication functionaliy of calculator",(){
-       Calculator calculator=Calculator();
-       int result=calculator.multiply(4, 5);
-       expect(result, 20);
-       expect(result, isNonZero);
-     });
-
-   });
-
+    test("Test Multiplication functionality of calculator", () {
+      Calculator calculator = Calculator();
+      int result = calculator.multiply(4, 5);
+      expect(result, 20);
+      expect(result, isNonZero);
+    });
+  });
 }
